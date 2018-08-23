cb83cc2dda916d48a7f7453f152b84590dd70d47
wongprom <jimmybackstrom@hotmail.com>
2018-08-23 14:30:31 +0200

    Got/define elements from index.html

---
+++ app.js
@@ -0,0 +1,6 @@
+// Get Elements from index.html
+const form = document.querySelector("#task-form");
+const tasklist = document.querySelector("colaction");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#tasks");
