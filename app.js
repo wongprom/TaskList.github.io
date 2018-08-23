d7c7d17970e79f93214f531ab521538a9d57bd11
wongprom <jimmybackstrom@hotmail.com>
2018-08-23 15:44:53 +0200

    Add a task
    Create li, add a className, appended textNode to li. Added icon to html with js. Finaly appended li to ul while press the Add Task.

--- app.js
+++ app.js
@@ -19,5 +19,23 @@ function addTask(e) {
     alert('Add A Task');
   }
 
+  //Create li Element
+  const li = document.createElement('li');
+  //Add A Class
+  li.className = 'collection-item';
+  //Create Text Node And Append It To li
+  li.appendChild(document.createTextNode(taskInput.value));
+  //Create New Link Element. The X To The Right
+  const link = document.createElement('a');
+  //Add A class To The New Element. class secondary-content Needs To Get The Icon To The Right Of A List Item When You Use The Front-End FrameWork Metirializer
+  link.className = 'delete-item secondary-content';
+  //Add Icon To The html
+  link.innerHTML = '<i class = "fa fa-remove"></i>';
+  //Append The link To li
+  li.appendChild(link);
+
+  //Append li To ul
+  tasklist.appendChild(li);
+
   e.preventDefault();
 }
