1e2786f457e4b9089c8eafd552b84569b1a38b6b
wongprom <jimmybackstrom@hotmail.com>
2018-08-23 15:16:17 +0200

    Prevent reload of page. Alert when try to sen a embty new task
    If Input value is empty, show alert. Use preventDefault method to prevent reload of page.

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-// Get Elements from index.html
-const form = document.querySelector("#task-form");
-const tasklist = document.querySelector("colaction");
-const clearBtn = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
-const taskInput = document.querySelector("#tasks");
+// Get Elements From index.html
+const form = document.querySelector('#task-form');
+const tasklist = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load All Event Listeners
+loadAllEventListeners();
+
+function loadAllEventListeners() {
+  //Add task event
+  form.addEventListener('submit', addTask);
+}
+
+//Add Task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add A Task');
+  }
+
+  e.preventDefault();
+}
