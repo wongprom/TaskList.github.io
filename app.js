18e76a9f33df2a02ba8f2da9c0d42613efdaad73
wongprom <jimmybackstrom@hotmail.com>
2018-08-24 00:51:36 +0200

    Filter/search trough tasks

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   ulTaskList.addEventListener('click', removeTask);
   //Clear all tasks event
   clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -62,4 +64,18 @@ function clearTasks() {
   while (ulTaskList.firstChild) {
     ulTaskList.firstChild.remove();
   }
+}
+
+//Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
