fa6e935f75e1097916b07a165964965ba9f493e2
wongprom <jimmybackstrom@hotmail.com>
2018-08-28 11:39:11 +0200

    Bug. Prevent to send an empty task

--- app.js
+++ app.js
@@ -56,33 +56,32 @@ function getTasks() {
 function addTask(e) {
   if (taskInput.value === '') {
     alert('Add a task');
+  } else {
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+    // Append li to ul
+    ulTaskList.appendChild(li);
+
+    //Stor in localStorage
+    storeTaskInLocalStorage(taskInput.value);
+
+    // Clear input
+    taskInput.value = '';
+
+    e.preventDefault();
   }
-
-  // Create li element
-  const li = document.createElement('li');
-  // Add class
-  li.className = 'collection-item';
-  // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
-  const link = document.createElement('a');
-  // Add class
-  link.className = 'delete-item secondary-content';
-  // Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append the link to li
-  li.appendChild(link);
-
-  // Append li to ul
-  ulTaskList.appendChild(li);
-
-  //Stor in localStorage
-  storeTaskInLocalStorage(taskInput.value);
-
-  // Clear input
-  taskInput.value = '';
-
-  e.preventDefault();
 }
 
 //Store task in localStorage
